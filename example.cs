b145162d617e9200af4f370d171016eca47324d8
giyoonhan <gh336@cornell.edu>
2016-02-29 01:26:06 -0500

    Added files

---
+++ example.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class testingscript : MonoBehaviour {
+
+    private VoiceRec.VoiceClass obj;
+    private string word;
+
+    void Start () {
+        obj = new VoiceRec.VoiceClass();
+        //set of commands : replace with set of commands to be recognized
+        string[] str = { "left", "right", "down", "up" };
+        obj.initRecog(str);
+	}
+	
+	void Update () {
+        word = obj.getWord();
+        if (word != null && word != "")
+        {
+            switch (word)
+            {
+                case "left":
+                    print("Voice input : left");
+                    break;
+                case "right":
+                    print("Voice input : right");
+                    break;
+                case "down":
+                    print("Voice input : down");
+                    break;
+                case "up":
+                    print("Voice input : up");
+                    break;
+            }
+        }
+	}
+}
